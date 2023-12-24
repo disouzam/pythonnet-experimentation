49aa8eb5bb91fd3a8041cc4bc45587031be2fb81
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-24 16:01:30 -0300

    feat: Initial commit with a blank new project

---
+++ HelloWorldConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
