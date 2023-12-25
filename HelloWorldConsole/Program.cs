4cad9c8139f0af1f02c1d71dfa32c4eac280e7c0
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-25 00:26:32 -0300

    feat: First sucessful implementation of a call to a script from disk
    - The script is the same as the one available in file https://github.com/disouzam/ironPython-experimentation/blob/main/IronPythonIntro/HelloWorld/PythonScripts/scriptFile.py
    - The idea was the same: get the handle for a function defined in Python script and execute it inside C# code
    - The syntax was not that different from what was learned in IronPython case

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -3,6 +3,7 @@
 #endif
 
 using System;
+using System.IO;
 
 using Python.Runtime;
 
@@ -17,6 +18,22 @@ internal static class Program
 #endif
         Runtime.PythonDLL = @"C:\Program Files\Python312\python312.dll";
         PythonEngine.Initialize();
+
+        var basePath = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory);
+        var scriptFilePath = Path.Combine(basePath.FullName, "scriptFile.py");
+
+        var scriptContent = File.ReadAllText(scriptFilePath, System.Text.Encoding.UTF8);
+
+        using(Py.GIL())
+        {
+            PythonEngine.Exec(scriptContent);
+
+            using var scope = Py.CreateScope();
+            scope.Exec(scriptContent);
+            dynamic greetings = scope.Get("greetings");
+            Console.WriteLine(greetings("world of Python.NET Nuget package"));
+        }
+
         PythonEngine.Shutdown();
         Console.WriteLine("Finished execution of console app.");
     }
---
+++ HelloWorldConsole/scriptFile.py
@@ -0,0 +1,2 @@
+def greetings(name):
+    return 'Hello ' + name.title() + '! A milestone has been achieved!' 
