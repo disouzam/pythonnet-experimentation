309a49385ee5c917347457599b9e1678ca79c6ed
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-25 15:46:28 -0300

    feat: Adjusted class Serialization4.cs and checked its behavior - working fine

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -15,11 +15,11 @@
 #endif
 
 #if !SERIALIZATION3
-#define SERIALIZATION3
+// #define SERIALIZATION3
 #endif
 
 #if !SERIALIZATION4
-//#define SERIALIZATION4
+#define SERIALIZATION4
 #endif
 
 #if !SERIALIZATION5
