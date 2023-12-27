56bb4febca37d9214ecd1db4d23f41187830b0fa
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2023-12-27 15:29:00 -0300

    refactor: Added new statements in serialization6.py to get the correct syntax of Numpy arrays handling

--- HelloWorldConsole/PythonScripts/serialization6.py
+++ HelloWorldConsole/PythonScripts/serialization6.py
@@ -1,9 +1,14 @@
 import pickle
+import numpy
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import make_regression
 
 # generate regression dataset
 X, y = make_regression(n_samples=100, n_features=3, noise=0.1, random_state=1)
+
+shapeOfX = numpy.shape(X)
+
+x0 = X[0]
 
 with open("regressionX.pkl", "wb") as f:
     pickle.dump(X, f)
@@ -14,6 +19,10 @@ with open("regressionY.pkl", "wb") as f:
 # train regression model
 linear_model = LinearRegression()
 linear_model.fit(X, y)
+
+coef0 = linear_model.coef_[0]
+coef1 = linear_model.coef_[1]
+coef2 = linear_model.coef_[2]
 
 print("Prediction initiated")
 print(linear_model.predict(X))
@@ -26,6 +35,8 @@ print('Model score : ', linear_model.score(X, y))
 
 with open("linear_regression.pkl", "wb") as f:
     pickle.dump(linear_model, f)
+
+
 
 with open("linear_regression.pkl", "rb") as f:
     unpickled_linear_model = pickle.load(f)
